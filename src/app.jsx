a4460761a3bf1d8b9801468f4ecbe21c6742cba3
Josiah Swab <josiahjswab@gmail.com>
2019-09-25 11:43:22 -0700

    JSX added.

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,51 @@ class App extends Component {
   }
 
   render() {
+    <div class="container">
+      <div class="row">
+        <div class="column-due">
+          <div class="panel">
+            <div class="panel-head">Header Text
+              <div class="panel-body">This is the total due.
+                <input type="number" name="" id="" placeholder="amount due"/>
+              </div>
+              <div class="panel-body">
+                <input type="number" name="" id="" placeholder="amount received"/>
+              </div>
+              <div class="panel-footer">
+                <button class="button">HITME</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="column-change">
+          <div class="panel">
+            <div class="panel-head">
+              This is change amount to return
+            </div>
+            <div class="panel-row">
+              <div class="panel-column"></div>
+              <div class="panel-column"></div>
+              <div class="panel-column"></div>
+              <div class="panel-column"></div>
+            </div>
+            <div class="panel-row">
+              <div class="panel-column"></div>
+              <div class="panel-column"></div>
+              <div class="panel-column"></div>
+              <div class="panel-column"></div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   }
 }
 
 export default App;
+
+
+
